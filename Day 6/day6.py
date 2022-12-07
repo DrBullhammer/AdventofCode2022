a0206863cb6df811429a49b4fc43c3aa242b9980
DrBullhammer <66756682+DrBullhammer@users.noreply.github.com>
2022-12-06 22:46:18 -0500

    Add files via upload

--- Day 6/day6.py
+++ Day 6/day6.py
@@ -3,6 +3,11 @@ with open('day6.txt') as file:
 
 
 for i in range(len(data)):
+    if i-3 >= 0 and len(set([data[i-x] for x in range(4)])) == 4:
+        print(i+1)
+        break
+
+for i in range(len(data)):
     if i-13 >= 0 and len(set([data[i-x] for x in range(14)])) == 14:
         print(i+1)
         break
