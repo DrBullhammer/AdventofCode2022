ae7db5cdcd56332edabdab424df94e39b652851e
DrBullhammer <66756682+DrBullhammer@users.noreply.github.com>
2022-12-10 15:35:58 -0500

    Add files via upload

--- Day10/day10.py
+++ Day10/day10.py
@@ -20,7 +20,6 @@ for i in data:
 signal_strengths = (cycle_value[20] *20) + (cycle_value[60] * 60) + (cycle_value[100] * 100) + (cycle_value[140] * 140) + (cycle_value[180] * 180) + (cycle_value[220] * 220)
 print(signal_strengths)
 
-
 row1 = []
 row2 = []
 row3 = []
@@ -28,12 +27,12 @@ row4 = []
 row5 = []
 row6 = []
 
-row_out1 = []
-row_out2 = []
-row_out3 = []
-row_out4 = []
-row_out5 = []
-row_out6 = []
+row_out1 = ''
+row_out2 = ''
+row_out3 = ''
+row_out4 = ''
+row_out5 = ''
+row_out6 = ''
 
 x = 0
 while x < len(cycle_value):
@@ -54,49 +53,49 @@ while x < len(cycle_value):
 a = 0
 while a < 40:
     if a-1 <= row1[a] <= a+1:
-        row_out1.append('#')
+        row_out1 += '#'
     else:
-        row_out1.append('.')
+        row_out1 += '.'
     a += 1
 
 b = 0
 while b < 40:
     if b-1 <= row2[b] <= b+1:
-        row_out2.append('#')
+        row_out2 += '#'
     else:
-        row_out2.append('.')
+        row_out2 += '.'
     b += 1
 
 c = 0
 while c < 40:
     if c-1 <= row3[c] <= c+1:
-        row_out3.append('#')
+        row_out3 += '#'
     else:
-        row_out3.append('.')
+        row_out3 += '.'
     c += 1
 
 d = 0
 while d < 40:
     if d-1 <= row4[d] <= d+1:
-        row_out4.append('#')
+        row_out4 += '#'
     else:
-        row_out4.append('.')
+        row_out4 += '.'
     d += 1
 
 e = 0
 while e < 40:
     if e-1 <= row5[e] <= e+1:
-        row_out5.append('#')
+        row_out5 += '#'
     else:
-        row_out5.append('.')
+        row_out5 += '.'
     e += 1
 
 f = 0
 while f < 40:
     if f-1 <= row6[f] <= f+1:
-        row_out6.append('#')
+        row_out6 += '#'
     else:
-        row_out6.append('.')
+        row_out6 += '.'
     f += 1
 
 print(row_out1)
@@ -105,10 +104,3 @@ print(row_out3)
 print(row_out4)
 print(row_out5)
 print(row_out6)
-
-'''print(row1)
-print(row2)
-print(row3)
-print(row4)
-print(row5)
-print(row6)'''
